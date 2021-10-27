a7290ce7566406049c90d5cd1ba12ca00aeff739
smackflad <ask.kalamakhs@hotmail.com>
2021-10-27 15:43:23 +0300

    added some comments

--- src/bkTrees/appMatching/hammingDistance.cpp
+++ src/bkTrees/appMatching/hammingDistance.cpp
@@ -6,13 +6,12 @@
 
 using bud::string;
 
-unsigned long getHamming(string f, string s){
+unsigned long getHamming(string f, string s){//simply for each letter (since we can assume that there are the same length)
     unsigned fin =0;
     for(unsigned i=0;i<f.size();i++){
-        if(f[i] != s[i]){
+        if(f[i] != s[i]){//check if they are the same or not.
             fin++;
         }
     }
-    // std::cout << f.m_text << " " << fin << std::endl;
     return fin;
 }
